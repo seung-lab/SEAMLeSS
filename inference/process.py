13119ee2806d2a37ca61e6a637a1100a468a17dc
Eric Mitchell <eam6@seungworkstation1000.Princeton.EDU>
2018-03-13 16:08:50 -0500

    Added new model, minor refactor to process to compensate for pyramids trained at mip other than 0.

--- inference/process.py
+++ inference/process.py
@@ -4,12 +4,16 @@ from PyramidTransformer import PyramidTransformer
 
 class Process(object):
     """docstring for Process."""
-    def __init__(self, path='model/1_6_3.pt', cuda=False):
+    def __init__(self, path='model/2_5_2.pt', cuda=False):
         super(Process, self).__init__()
         self.cuda = cuda
-        self.model = PyramidTransformer().load(archive_path=path, cuda=cuda)
-
+        self.model = PyramidTransformer().load(archive_path=path, height=5, skips=2, cuda=cuda)
+        self.mip = 5 # hardcoded to be the mip that the model was trained at
+        
     def process(self, s, t, level=0, crop=0):
+        if level < self.mip + self.model.pyramid.skip:
+            return None
+        level -= self.mip
         x = torch.from_numpy(np.stack((s, t), axis=1))
         if self.cuda:
             x = x.cuda()
@@ -22,7 +26,19 @@ class Process(object):
 #Simple test
 if __name__ == "__main__":
     a = Process()
-    s = np.ones((8,256,256), dtype=np.float32)
-    t = np.ones((8,256,256), dtype=np.float32)
+    s = np.ones((2,256,256), dtype=np.float32)
+    t = np.ones((2,256,256), dtype=np.float32)
+
+    flow = a.process(s, t, level=8)
+    assert flow.shape == (2,256,256,2)
+
+    flow = a.process(s, t, level=7, crop=10)
+    assert flow.shape == (2,236,236,2)
+
+    flow = a.process(s, t, level=6)
+    assert flow == None
+
     flow = a.process(s, t, level=5, crop=10)
-    print(flow.shape) #expected output (8,256,256,2)
+    assert flow == None
+
+    print 'All tests passed.'
