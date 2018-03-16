6219481c86b0412422acfafbf79b8379554e3933
Sergiy Popovych <popovych@princeton.edu>
2018-03-16 10:50:39 -0400

    save

--- inference/renderer.py
+++ inference/renderer.py
@@ -1,6 +1,6 @@
 from aligner import Aligner, BoundingBox
 from copy import deepcopy
-from time import time
+
 s = 512
 t = 5120
 mip = 7
@@ -14,28 +14,15 @@ crop = 64
 influence_bbox = deepcopy(patch_bbox)
 influence_bbox.uncrop(crop, mip=mip)
 
-a = Aligner('model/2_5_2.pt', (64, 64), 1024, 32, 9, 7, 'gs://neuroglancer/pinky40_alignment/prealigned', 'gs://neuroglancer/nflow_tests/p40_pre', move_anchor=False)
+a = Aligner('model/2_5_2.pt', (64, 64), 1024, 32, 6, 6, 'gs://neuroglancer/pinky40_alignment/prealigned', 'gs://neuroglancer/nflow_tests/p40_pre', move_anchor=True)
 
-for z in range(10, 100):
-  a.set_processing_chunk_size((64, 64))
-  start = time()
-  a.align_ng_stack(z, z + 1, patch_bbox)
-  end = time()
-  print ("Aligmnent: {} sec".format(end-start))
-  c = 2048
-  m = 2
-  start = time()
-  for x in range(0, 6):
-    s = max(int(2048/(2**x)), 64)
-    a.set_processing_chunk_size((s, s))
-    a.render(z + 1, patch_bbox, mip=m+x)
-  end = time()
-  print ("Render: {} sec".format(end-start))
+a.align_ng_stack(10, 10, patch_bbox)
 #a.compute_section_pair_residuals(source_z, target_z, patch_bbox)
 #a.save_aggregate_flow(source_z, patch_bbox, mip=6)
 #a.save_aggregate_flow(source_z, patch_bbox, mip=5)
 #a.save_aggregate_flow(source_z, patch_bbox, mip=4)
-#a.render(source_z, patch_bbox, mip=9)
+#for s in range(12, 21):
+#    a.render(s, patch_bbox, mip=2)
 #a.render(source_z, patch_bbox, mip=8)
 #a.render(source_z, patch_bbox, mip=2)
 #a.render(source_z, patch_bbox, mip=7)
