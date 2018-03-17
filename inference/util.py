87d0d72afd916155ca27c17bd82bfe113787bd4f
supersergiy <sergiy.popovich@gmail.com>
2018-03-17 13:20:33 -0400

    refactor, new render

--- inference/util.py
+++ inference/util.py
@@ -12,8 +12,20 @@ def upsample(data_3d, factor):
     result = m(torch.from_numpy(data_4d))
     return result.data.numpy()[:, 0, :, :]
 
+def upsample_flow(data_4d, factor):
+    result = np.stack((upsample(data_4d[:, :, :, 0], factor),
+                       upsample(data_4d[:, :, :, 1], factor)), axis=3)
+    return result
+
+def downsample_mip(data_3d, factor):
+    m = nn.AvgPool2d(2, stride=2)
+    data_4d = np.expand_dims(data_3d, axis=1)
+    result = m(torch.from_numpy(data_4d))
+    return result.data.numpy()[:, 0, :, :]
+
 def warp(data, flow):
     td = torch.from_numpy(np.expand_dims(data, 0))
     tf = torch.from_numpy(flow)
     y = F.grid_sample(td, tf)
     return y.data.numpy()[0, 0]
+
