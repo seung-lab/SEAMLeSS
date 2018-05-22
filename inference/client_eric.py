6cb6dfb3d79f2212cbabb5830ac5c7f76aa041f9
Eric Mitchell <eam6@seungworkstation1000.Princeton.EDU>
2018-05-22 18:20:44 -0500

    Added new crack model.

--- inference/client_eric.py
+++ inference/client_eric.py
@@ -18,7 +18,7 @@ y_size = 40960
 bbox = BoundingBox(v_off[0], v_off[0]+x_size, v_off[1], v_off[1]+y_size, mip=0, max_mip=9)
 
 stack_start = 18
-stack_size  = 30
-a.align_ng_stack(stack_start, stack_start+stack_size, bbox, move_anchor=False)
+stack_size  = 100
+a.align_ng_stack(stack_start, stack_start+stack_size, bbox, move_anchor=True)
 stack_start += stack_size
 a.align_ng_stack(stack_start, stack_start+stack_size, bbox, move_anchor=False)
