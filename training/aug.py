Major refactor, adding use of ceil for masks, adding mask helpers, and fixing unflow penalty. Fixed nan issue regarding weight normalization.

training/aug.py
@@ -185,7 +185,7 @@ def displace_slice(stack, slice_idx, aux, displacement=32):
     stack, grid = rotate_and_scale(stack, None)
     aux = [rotate_and_scale(a, grid=grid)[0].squeeze() for a in aux]
     return stack, aux
-                      
+
 def random_rect_mask(size):
     dimx = random.randint(1,size[-2]/2)
     dimy = random.randint(1,size[-2]/2)
@@ -198,8 +198,7 @@ def random_rect_mask(size):
     dx = random.randint(1, upper_bound)
     dy = random.randint(1, upper_bound)
     off_centered = Variable(translate(rotated_centered.data, dx, dy))
-    output, _ = rotate_and_scale(off_centered, None, 0)
-    output[output > 0] = 1
+    output = torch.ceil(rotate_and_scale(off_centered, None, 0)[0])
     return output.byte()
 
 def aug_input(x, factor=2.0):

training/vis.py
@@ -7,10 +7,14 @@ def norm(stack, factor=1):
     return factor * ((stack - np.min(stack)) / (np.max(stack) - np.min(stack)))
 
 def visualize_outputs(path, outputs):
+    if outputs is None:
+        print('Skipping visualization of empty outputs.')
+        return
+
     src = outputs['input_src'].unsqueeze(0).unsqueeze(0) if 'input_src' in outputs else None
     target = outputs['input_target'].unsqueeze(0).unsqueeze(0) if 'input_target' in outputs else None
     pred = outputs['pred'] if 'pred' in outputs else None
-    field = outputs['field'] if 'field' in outputs else None
+    field = outputs['rfield'] if 'rfield' in outputs else None
     residuals = outputs['residuals'] if 'residuals' in outputs else None
     similarity_error_field = outputs['similarity_error_field'] if 'similarity_error_field' in outputs else None
     smoothness_error_field = outputs['smoothness_error_field'] if 'smoothness_error_field' in outputs else None