c7fb9da05141d9777daf4583418bcbe0293da084
Eric Mitchell <eam6@seungworkstation1003.princeton.edu>
2018-07-17 11:02:12 -0400

    Added support for in-terminal Neuroglancer links; added missing dependencies to requirements.txt.

--- inference/link_builder.py
+++ inference/link_builder.py
@@ -1,4 +1,6 @@
-def ng_link(out_name, out_full_path, source_name, source_full_path, x_center, y_center, z_start):
+from pyshorteners import Shortener
+
+def ng_link(out_name, out_full_path, source_name, source_full_path, x_center, y_center, z_start, shorten=True):
     link = "https://neuromancer-seung-import.appspot.com/#!{'layers':{'__OUT_NAME__':{'type':'image'_'source':'__OUT_FULL_PATH__'}_'__SOURCE_NAME__':{'type':'image'_'source':'__SOURCE_FULL_PATH__'_'visible':false}}_'navigation':{'pose':{'position':{'voxelSize':[4_4_40]_'voxelCoordinates':[__X_CENTER_____Y_CENTER_____Z_START__]}}_'zoomFactor':150}_'layout':'xy'}"
     link = link.replace('__OUT_NAME__', out_name)
     link = link.replace('__OUT_FULL_PATH__', out_full_path)
@@ -7,4 +9,8 @@ def ng_link(out_name, out_full_path, source_name, source_full_path, x_center, y_
     link = link.replace('__X_CENTER__', str(x_center))
     link = link.replace('__Y_CENTER__', str(y_center))
     link = link.replace('__Z_START__', str(z_start))
+    if shorten:
+        shortener = Shortener('Bitly', bitly_token='603cd89366c18495e291a71c28022b0ed17035aa')
+        link = shortener.short(link)
+
     return link
