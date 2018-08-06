d274f0de02d497bf248f67d46a302c09d96f453f
bnehoran <bnehoran@users.noreply.github.com>
2018-08-06 15:04:54 -0400

    Add option to run inference on consecutive pairs of input slices (#2)

--- inference/client_eric.py
+++ inference/client_eric.py
@@ -22,7 +22,8 @@ parser.add_argument('--ye', type=int)
 parser.add_argument('--stack_size', type=int, default=100)
 parser.add_argument('--zs', type=int)
 parser.add_argument('--no_anchor', action='store_true')
-parser.add_argument('--no_flip_average', action='store_true')
+parser.add_argument('--no_flip_average', help='disable flip averaging, on by default (flip averaging is used to eliminate drift)', action='store_true')
+parser.add_argument('--run_pairs', help='only run on consecutive pairs of input slices, rather than sequentially aligning a whole stack', action='store_true')
 args = parser.parse_args()
 
 out_name = args.out_name
@@ -63,7 +64,7 @@ print('NG link:', ng_link(out_name, 'precomputed://' + 'gs://neuroglancer/seamle
 
 a = Aligner(model_path, max_displacement, edge_crop, mip_range, high_mip_chunk, source,
             'gs://neuroglancer/seamless/{}_{}'.format(model_name, out_name), render_low_mip=render_mip, render_high_mip=max_mip,
-            skip=0, topskip=0, size=args.size, should_contrast=should_contrast, num_targets=num_targets, flip_average=not args.no_flip_average)
+            skip=0, topskip=0, size=args.size, should_contrast=should_contrast, num_targets=num_targets, flip_average=not args.no_flip_average, run_pairs=args.run_pairs)
 
 bbox = BoundingBox(v_off[0], v_off[0]+x_size, v_off[1], v_off[1]+y_size, mip=0, max_mip=max_mip)
 
