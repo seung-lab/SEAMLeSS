8cc8974a43ffb4394f3b15867fa0b9de96237806
Thomas Macrina <tmacrina@princeton.edu>
2018-08-24 11:32:47 -0400

    Update eval/inspect for python neuroglancer package API

--- eval/inspect/field.py
+++ eval/inspect/field.py
@@ -1,32 +1,61 @@
 from cloudvolume import CloudVolume
 from cloudvolume.lib import Bbox, Vec
-import correspondences
+from correspondences import Controller
 import numpy as np
 
 from os.path import join
 
 class Field():
   """Display vector fields from CloudVolume in neuroglancer for inspection.
+
+    Args:
+    * path: CloudVolume path to the root directory of a SEAMLeSS run
+            images will be at path/image and vectors at path/vec
+    * mip: mip level of the vectors to be displayed
+
+    Example:
+    ```
+      from field import Field
+      path = "gs://neuroglancer/seamless/matriarch_tile7_pinky100_pairs_write_res_v13"
+      F = Field(path, mip=5)
+      # open link provided, navigate to z=1345, & zoom in to an area of interest
+      F.inspect(F.get_bbox())
+    ```
   """
-  def __init__(self, path, mip=5, port=9999):
-    self.controller = correspondences.controller(port)
+  def __init__(self, path, mip=5, vec_name='vec'):
     self.mip = mip
-    self.path = path
+    self.root = path
+    self.img_path = None
+    self.vec_path = None
     self.x_cv = None
     self.y_cv = None
-    self.set_path(path)
+    self.controller = Controller()
+    self.set_vector_path(join(path, vec_name))
+    self.set_image_path(join(path, 'image'))
 
-  def set_path(self, path):
-    self.path = path
+  def set_image_path(self, path):
+    self.img_path = path
+    layer = {'name': 'image',
+              'url': join('precomputed://', self.img_path)}
+    self.controller.set_layer(layer)
+    print(self.controller.viewer)
+
+  def set_vector_path(self, path):
+    self.vec_path = path
     self.set_mip(self.mip)
 
   def set_mip(self, mip):
     self.mip = mip
-    self.x_cv = CloudVolume(join(self.path, str(mip), 'x'), mip=mip, fill_missing=True)
-    self.y_cv = CloudVolume(join(self.path, str(mip), 'y'), mip=mip, fill_missing=True)
+    self.x_cv = CloudVolume(join(self.vec_path, str(mip), 'x'), 
+                            mip=mip, fill_missing=True)
+    self.y_cv = CloudVolume(join(self.vec_path, str(mip), 'y'), 
+                            mip=mip, fill_missing=True)
 
   def display(self, pts):
     self.controller.set(pts)
+
+  def get_bbox(self):
+    return self.controller.get_bbox()
 
   def get_abs_residuals(self, bbox, bbox_mip=0):
     bbox_adj = Vec(2**(self.mip-bbox_mip), 2**(self.mip-bbox_mip), 1)
@@ -40,7 +69,7 @@ class Field():
     x, y = self.get_abs_residuals(bbox, bbox_mip)
     pts = self.to_points(x, y, bbox.minpt, self.mip)
     self.display(pts)
-    return pts
+    # return pts
 
   def to_points(self, x, y, offset, mip):
     """Convert Neuroflow vec fields to list of points for neuroglancer
@@ -54,9 +83,5 @@ class Field():
         dx = int(np.round(y[n,m]))
         dy = int(np.round(x[n,m]))
         dz = 1
-        pts.extend([(x0, y0, z0), (x0+dx, y0+dy, z0+dz)])
+        pts.append([(x0, y0, z0), (x0+dx, y0+dy, z0+dz)])
     return pts
-
-# from field import Field
-# F = Field("gs://neuroglancer/seamless/matriarch_tile7_drosophila_pairs_v0/vec", mip=5)
-# F.inspect(F.controller.get_bbox())
