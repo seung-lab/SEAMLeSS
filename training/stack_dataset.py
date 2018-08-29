d6d09d09a32cc2bc2ec291a20be1b3bc6c3cb6c4
Thomas Macrina <tmacrina@princeton.edu>
2018-08-29 11:59:01 -0400

    Rework stack_dataset to deliver consecutive image pair

--- training/stack_dataset.py
+++ training/stack_dataset.py
@@ -5,42 +5,45 @@ with warnings.catch_warnings():
 
 import torch
 import numpy as np
-from torch.utils.data import Dataset
-import matplotlib
-matplotlib.use('Agg')
+from torch.utils.data import Dataset, ConcatDataset
+
+def compile_dataset(h5_paths):
+    datasets = []
+    for h5_path in h5_paths:
+        datasets.extend(h5_to_dataset_list(h5_path))
+    return ConcatDataset(datasets)
+
+def h5_to_dataset_list(h5_path):
+    """Create a list of StackDatasets from H5 file created by gen_stack.py
+    """
+    stacks = []
+    h5f = h5py.File(h5_path, 'r')
+    for k in h5py.keys:
+        d = h5py[k]
+        for i in range(d.shape[0])
+            stacks.append(StackDataset(d[i:i+1]))
+    return stacks
 
 class StackDataset(Dataset):
-    def __init__(self, source_h5, mip=-1):
-        self.h5f = h5py.File(source_h5, 'r')
+    """Deliver consecutive image pairs from 3D image stack 
 
-        self.datasets = []
-        for k in self.h5f.keys():
-            if 'pinky' not in k:
-                self.datasets.append(self.h5f[k])
-            else:
-                print('Throwing out pinky...')
-        self.lengths = [d.shape[0] for d in self.datasets]
-        self.clengths = np.cumsum(self.lengths)
-        self.length = sum(self.lengths)
-        self.mip = mip
+    Args:
+        stack (4D ndarray): 1xZxHxW image array
+    """
 
-    def map_total_index(self, idx):
-        chunks = [idx < l for l in self.clengths]
-        assert True in chunks, 'Index {} not contains in Stack Dataset with lengths {}.'.format(idx, self.lengths)
-        dataset_idx = chunks.index(True)
-        sub_idx = idx if dataset_idx == 0 else idx - self.clengths[dataset_idx - 1]
-        return (dataset_idx, sub_idx)
+    def __init__(self, stack):
+        self.stack = stack
 
     def __len__(self):
+        # N-1 consecutive image pairs
+        return self.stack.shape[1]-1
 
-        return self.length
+    def toFloatTensor(self, image):
+        t = torch.FloatTensor(image) / 255.
+        return Variable(t, requires_grad=False).cuda()
 
-    def __getitem__(self, idx):
-        dataset_idx, sub_idx = self.map_total_index(idx)
-        t = torch.FloatTensor(self.datasets[dataset_idx][sub_idx]) / 255.
-
-        output = { 'X': t }
-        if self.mip != -1:
-            output['mip'] = self.mip
-
-        return output
+    def __getitem__(self, k):
+        src = self.toFloatTensor(self.stack[k])
+        tgt = self.toFloatTensor(self.stack[k+1])
+        X = {'src': src, 'tgt': tgt}
+        return X
