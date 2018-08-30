816ab9f9296f42ab30172d28bb06f0f0233e96f9
Thomas Macrina <tmacrina@princeton.edu>
2018-08-30 13:16:02 -0400

    Update field.py to handle combined xy vectors

--- eval/inspect/field.py
+++ eval/inspect/field.py
@@ -27,8 +27,7 @@ class Field():
     self.root = path
     self.img_path = None
     self.vec_path = None
-    self.x_cv = None
-    self.y_cv = None
+    self.cv = None
     self.controller = Controller()
     self.set_vector_path(join(path, vec_name))
     self.set_image_path(join(path, 'image'))
@@ -46,10 +45,8 @@ class Field():
 
   def set_mip(self, mip):
     self.mip = mip
-    self.x_cv = CloudVolume(join(self.vec_path, str(mip), 'x'), 
-                            mip=mip, fill_missing=True)
-    self.y_cv = CloudVolume(join(self.vec_path, str(mip), 'y'), 
-                            mip=mip, fill_missing=True)
+    path = join(self.vec_path, str(mip))
+    self.cv = CloudVolume(path, mip=mip, fill_missing=True)
 
   def display(self, pts):
     self.controller.set(pts)
@@ -60,28 +57,27 @@ class Field():
   def get_abs_residuals(self, bbox, bbox_mip=0):
     bbox_adj = Vec(2**(self.mip-bbox_mip), 2**(self.mip-bbox_mip), 1)
     mip_bbox = bbox // bbox_adj
-    x = self.x_cv[mip_bbox.to_slices()]
-    y = self.y_cv[mip_bbox.to_slices()]
-    return x, y
+    res = self.cv[mip_bbox.to_slices()][:,:,0,:]
+    return res
 
   def inspect(self, bbox, bbox_mip=0):
     print('Inspecting {0}'.format(bbox))
-    x, y = self.get_abs_residuals(bbox, bbox_mip)
-    pts = self.to_points(x, y, bbox.minpt, self.mip)
+    res = self.get_abs_residuals(bbox, bbox_mip)
+    pts = self.to_points(res, bbox.minpt, self.mip)
     self.display(pts)
     # return pts
 
-  def to_points(self, x, y, offset, mip):
-    """Convert Neuroflow vec fields to list of points for neuroglancer
+  def to_points(self, res, offset, mip):
+    """Convert vec fields to list of points for neuroglancer
     """
     pts = []
-    for n in range(x.shape[0]):
-      for m in range(x.shape[1]):
+    for n in range(res.shape[0]):
+      for m in range(res.shape[1]):
         x0 = int(offset[0]) + n*(2**mip)
         y0 = int(offset[1]) + m*(2**mip)
         z0 = int(offset[2]-1)
-        dx = int(np.round(y[n,m]))
-        dy = int(np.round(x[n,m]))
+        dx = int(np.round(res[n,m.0]))
+        dy = int(np.round(res[n,m.1]))
         dz = 1
         pts.append([(x0, y0, z0), (x0+dx, y0+dy, z0+dz)])
     return pts
