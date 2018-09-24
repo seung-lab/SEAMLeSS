633a7d20bdddc643b1e39787a3bbf97265a49ca8
Sergiy Popovych <sergiy.popovich@gmail.com>
2018-09-24 13:45:20 -0500

    preemtibility fixed

--- inference/task_handler.py
+++ inference/task_handler.py
@@ -55,7 +55,7 @@ class TaskHandler:
       MessageBody=message_body
     )
 
-  def get_message(self, processing_time=3600):
+  def get_message(self, processing_time=90):
     response = self.sqs.receive_message(
       QueueUrl=self.queue_url,
       MaxNumberOfMessages=1,
