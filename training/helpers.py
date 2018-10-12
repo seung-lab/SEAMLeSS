4df253dc39ee83417acb6881e8a02c2e07411e59
Barak Nehoran <bnehoran@hotmail.com>
2018-10-12 04:21:11 -0400

    Checkout helpers.py from multi-gpu

--- training/helpers.py
+++ training/helpers.py
@@ -11,6 +11,7 @@ from functools import reduce
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 import matplotlib.cm as cm
 
 def compose_functions(fseq):
@@ -204,7 +205,7 @@ def save_chunk(chunk, name, norm=True):
         chunk[-10:,-10:] = 0
     plt.imsave(name + '.png', 1 - chunk, cmap='Greys')
 
-def gif(filename, array, fps=8, scale=1.0):
+def gif(filename, array, fps=2, scale=1.0):
     """Creates a gif given a stack of images using moviepy
     >>> X = randn(100, 64, 64)
     >>> gif('test.gif', X)
