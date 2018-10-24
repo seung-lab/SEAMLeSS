4b11a3832eb601e21ec0ca8be15eaabfc2df5b76
Thomas Macrina <tmacrina@princeton.edu>
2018-10-24 13:12:04 -0400

    Update/add vector voting helper methods

--- inference/vector_vote.py
+++ inference/vector_vote.py
@@ -2,6 +2,7 @@ import torch
 from torch import matmul, pow
 from torch.nn.functional import softmax
 from cloudvolume.lib import Bbox, Vec
+import math
 
 import util
 import argparse
@@ -15,7 +16,7 @@ def get_diffs(fields):
   diffs = []
   for i in range(len(fields)):
     for j in range(i+1, len(fields)):
-      diffs.append(dist(fields[i], fields[j])
+      diffs.append(dist(fields[i], fields[j]))
   return torch.cat(diffs, dim=0)
 
 def weight_diffs(diffs, T=1):
@@ -25,6 +26,7 @@ def compile_field_weights(W):
   m = W.shape[0]
   n = int((1 + math.sqrt(1 + 8*m)) / 2)
   C = torch.zeros((n,) +  W.shape[1:])
+  C = C.to(device=W.device)
   k = 0
   for i in range(n):
     for j in range(i+1, n):
@@ -33,18 +35,40 @@ def compile_field_weights(W):
       k += 1  
   return C / (n-1)
 
+def weighted_sum_fields(weights, fields):
+  """Created weighted sum of a list of fields
+  
+  Args:
+     weights: tensor with batch-size N
+     fields: N-size list of tensors
+  """
+  field = torch.cat(fields, dim=0)
+  field = field.permute(2,1,0,3)
+  weights = weights.permute(3,2,1,0)
+  # print('weighted_sum_fields weights.shape {0}'.format(weights.shape))
+  # print('weighted_sum_fields field.shape {0}'.format(field.shape))
+  return matmul(weights, field).permute(2,1,0,3)
+
+def vector_vote(fields, T=1):
+  diffs = get_diffs(fields)
+  diff_weights = weight_diffs(diffs, T=T)
+  field_weights = compile_field_weights(diff_weights)
+  return weighted_sum_fields(field_weights, fields)
+
 if __name__ == '__main__':
 
   parser = argparse.ArgumentParser(
-                    description='Create chunked pearson correlation image.')
+              description='Combine vector fields based on voting.')
   parser.add_argument('--field_paths', type=str, nargs='+', 
     help='List of CloudVolume paths to images')
   parser.add_argument('--weight_path', type=str,
-    help='CloudVolume path where weights written')
+    help='CloudVolume path where to write weights')
   parser.add_argument('--dst_path', type=str,
     help='CloudVolume path where output image written')
   parser.add_argument('--mip', type=int,
     help='MIP level of images to be used in evaluation')
+  parser.add_argument('--temperature', type=int, default=1,
+    help='softmax temperature')
   parser.add_argument('--bbox_start', nargs=3, type=int,
     help='bbox origin, 3-element int list')
   parser.add_argument('--bbox_stop', nargs=3, type=int,
@@ -66,17 +90,31 @@ if __name__ == '__main__':
     srcs.append(util.get_field_cloudvolume(path, mip=args.mip))
   dst = util.create_field_cloudvolume(args.dst_path, srcs[0][0].info, 
                                      args.mip, args.mip)
-  wts = util.get_cloudvolume(args.weight_path, mip=args.mip)
-  
+
+  wts = util.create_cloudvolume(args.weight_path, srcs[0][0].info, 
+                                     args.mip, args.mip)
+  wts.info['num_channels'] = 3
+  wts.commit_info()
+
   bbox = srcs[0][0].bbox_to_mip(bbox, args.bbox_mip, args.mip)
-  fields = util.get_field(srcs[0], bbox)
-  for src in srcs[1:]:
-    u = util.get_field(src, bbox)
-    fields = torch.cat([fields, u], dim=0)
-  fields = fields.permute(1,2,0,3)
-  w = util.to_tensor(util.get_image(wts, bbox))
-  w = w.permute(3,2,1,0)
-  v = matmul(w, fields)
-  v = v.permute(2,0,1,3)
-  util.save_field(dst, bbox, util.field_to_numpy(v))
+  fields = []
+  for src in srcs:
+    print('Loading field from {0}'.format(src[0].path))
+    f = util.get_field(src, bbox)
+    print('field shape {0}'.format(f.shape)) 
+    fields.append(f)
+
+  print('get_diffs')
+  diffs = get_diffs(fields)
+  print('diffs.shape {0}'.format(diffs.shape))
+  print('weight_diffs')
+  diff_weights = weight_diffs(diffs, T=args.temperature)
+  print('diff_weights.shape {0}'.format(diff_weights.shape))
+  field_weights = compile_field_weights(diff_weights)
+  print('field_weights.shape {0}'.format(field_weights.shape))
+  print('weighted_sum_fields')
+  field = weighted_sum_fields(field_weights, fields)
+  print('diff_weights.shape {0}'.format(diff_weights.shape))
+  util.save_image(wts, bbox, util.to_numpy(field_weights.permute(1,0,3,2)))
+  util.save_field(dst, bbox, util.field_to_numpy(field))
 
