018766de5b9ec237e72240c81871cd1ce12ee6ee
Thomas Macrina <tmacrina@princeton.edu>
2018-10-24 13:13:47 -0400

    Update aligner interfaces with abstracted args

--- inference/render_section.py
+++ inference/render_section.py
@@ -1,109 +1,17 @@
-import sys
-import torch
-from aligner import Aligner, BoundingBox
-from link_builder import ng_link
-import argparse
+from args import get_argparser, parse_args, get_aligner, get_bbox 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--source', type=str)
-parser.add_argument('--size', type=int, default=8)
-parser.add_argument('--skip', type=int, default=0)
-parser.add_argument('--model_path', type=str)
-parser.add_argument('--out_name', type=str)
-parser.add_argument('--mip', type=int)
-parser.add_argument('--render_mip', type=int)
-parser.add_argument('--should_contrast', type=int)
-parser.add_argument('--num_targets', type=int)
-parser.add_argument('--edge_pad', type=int, default=384)
-parser.add_argument('--max_displacement', 
-  help='the size of the largest displacement expected; should be 2^high_mip', 
-  type=int, default=2048)
-parser.add_argument('--max_mip', type=int, default=9)
-parser.add_argument('--xs', type=int)
-parser.add_argument('--xe', type=int)
-parser.add_argument('--ys', type=int)
-parser.add_argument('--ye', type=int)
-parser.add_argument('--stack_size', type=int, default=100)
-parser.add_argument('--src_z', type=int, help='z of source image')
-parser.add_argument('--tgt_z', type=int, help='z of target image')
-parser.add_argument('--no_anchor', action='store_true')
-parser.add_argument('--no_flip_average', 
-  help='disable flip averaging, on by default (flip averaging is used to eliminate drift)', 
-  action='store_true')
-parser.add_argument('--run_pairs', 
-  help='only run on consecutive pairs of input slices, rather than sequentially aligning a whole stack', 
-  action='store_true')
-parser.add_argument('--old_upsample', help='revert to the old pytorch upsampling (using align_corners=True)',
-  action='store_true')
-parser.add_argument('--old_vectors', help='expect the net to return vectors in the old vector field convention, '
-  'where -1 and 1 refer to the centers of the border pixels rather than the image edges.',
-  action='store_true')
-parser.add_argument('--ignore_field_init', help='do not initialize the field cloudvolume (already exists)',
-  action='store_true')
-parser.add_argument('--write_intermediaries', 
-  help='write encodings, residuals, & cumulative residuals to cloudvolumes', 
-  action='store_true')
-parser.add_argument('--upsample_residuals', 
-  help='upsample residuals & cumulative residuals when writing intermediaries; requires --write_intermediaries flag', 
-  action='store_true')
-args = parser.parse_args()
+def render(aligner, bbox, z):
+  aligner.total_bbox = bbox
+  aligner.zs = z
+  aligner.render_section_all_mips(z, bbox)
 
-out_name = args.out_name
-mip = args.mip
-render_mip = args.render_mip
-should_contrast = bool(args.should_contrast)
-num_targets = args.num_targets
-model_path = args.model_path
-model_name = model_path[model_path.rindex('/')+1:model_path.rindex('.')]
-max_displacement = args.max_displacement
-edge_pad  = args.edge_pad
-mip_range = (mip,mip)
-high_mip_chunk = (1024, 1024)
-source = args.source
-max_mip = args.max_mip
-xs = args.xs
-xe = args.xe
-ys = args.ys
-ye = args.ye
-src_z = args.src_z
-tgt_z = args.tgt_z
-v_off = (xs, ys, src_z)
-x_size = xe - xs
-y_size = ye - ys
-stack_size = args.stack_size
-out_cv = 'gs://neuroglancer/seamless/{}_{}'.format(model_name, out_name) 
+if __name__ == '__main__':
+  parser = get_argparser()
+  args = parse_args(parser) 
+  a = get_aligner(args)
+  bbox = get_bbox(args)
 
-if num_targets < 1:
-    print('num_targets must be > 0')
-    sys.exit(1)
+  for z in range(args.bbox_start[2], args.bbox_stop[2]): 
+    print('Rendering z={0}'.format(z))
+    render(a, bbox, z)
 
-print('Model name:', model_name)
-print('Tag:', out_name)
-print('Source:', source)
-print('Coordinates:', (args.xs, args.ys, args.tgt_z), (args.xe, args.ye, args.src_z))
-print('Mip:', mip)
-print('Contrast:', should_contrast)
-print('Max mip:', max_mip)
-print('NG link:', ng_link(out_name, 'precomputed://' + 'gs://neuroglancer/seamless/' + model_name+'_'+out_name+'/image', source[source.rindex('/')+1:], 'precomputed://' + source, (xs+xe)//2, (ys+ye)//2, tgt_z))
-
-a = Aligner(model_path, max_displacement, edge_pad, mip_range, high_mip_chunk,
-            source, out_cv, render_low_mip=render_mip, render_high_mip=max_mip,
-            skip=args.skip, topskip=0, size=args.size, should_contrast=should_contrast,
-            num_targets=num_targets, flip_average=not args.no_flip_average,
-            run_pairs=args.run_pairs,
-            write_intermediaries=args.write_intermediaries, 
-            upsample_residuals=args.upsample_residuals, old_upsample=args.old_upsample, 
-            old_vectors=args.old_vectors, ignore_field_init=args.ignore_field_init)
-
-bbox = BoundingBox(v_off[0], v_off[0]+x_size, v_off[1], v_off[1]+y_size, mip=0, max_mip=max_mip)
-if not a.check_all_params():
-  raise Exception("Not all parameters are set")
-#if not bbox.is_chunk_aligned(self.dst_ng_path):
-#  raise Exception("Have to align a chunkaligned size")
-
-a.total_bbox = bbox
-a.zs = src_z
-# for m in range(a.render_low_mip, a.high_mip):
-#   a.copy_section(a.src_ng_path, a.dst_ng_path, tgt_z, bbox, mip=m)
-# a.compute_section_pair_residuals(src_z, tgt_z, bbox)
-a.render_section_all_mips(src_z, bbox)
