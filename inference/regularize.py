52fa6984220673fd5726e74b917d5c9db2c3b463
Thomas Macrina <tmacrina@princeton.edu>
2018-12-10 15:56:15 -0500

    Make compose and regularize operate in blocks over z_range

--- inference/regularize.py
+++ inference/regularize.py
@@ -7,11 +7,20 @@ if __name__ == '__main__':
   parser = get_argparser()
   parser.add_argument('--compose_start', help='earliest section composed', type=int)
   parser.add_argument('--sigma', help='std of the bump function', type=float)
+  parser.add_argument('--compose_block',
+    help='block size before using a new compose_start',
+    type=int, default=0) 
   args = parse_args(parser) 
   a = get_aligner(args)
   bbox = get_bbox(args)
+
   mip = args.mip
+  block_size = args.compose_block // 2
+  # if args.compose_block == 0:
+    # args.compose_block = args.bbox_stop[2] - args.bbox_start[2]
+    # block_size = args.compose_block
 
-  z_range = range(args.bbox_start[2], args.bbox_stop[2])
-  a.regularize_z_chunkwise(z_range, args.compose_start, bbox, mip, sigma=args.sigma)
-
+  for block_start in range(args.bbox_start[2], args.bbox_stop[2], args.compose_block):
+    z_range = range(block_start + block_size, block_start + block_size + args.compose_block)
+    print('Regularizing for z_range {0}'.format(z_range))
+    a.regularize_z_chunkwise(z_range, block_start, bbox, mip, sigma=args.sigma)
