a5b22ab2ac5f0fa7851c7fab391fd1a056f021d1
Zhen Jia <zhenj@seungworkstation1002.princeton.edu>
2018-12-20 11:02:23 -0500

    modify render_low_mip.py

--- inference/render_low_mip.py
+++ inference/render_low_mip.py
@@ -18,14 +18,14 @@ if __name__ == '__main__':
   a.dst[0].add_composed_cv(args.bbox_start[2], inverse=False)
   field_k = a.dst[0].get_composed_key(args.bbox_start[2], inverse=False)
   field_cv= a.dst[0].for_read(field_k)
-  dst_cv = a.dst[0].for_write('dst_img_1')
+  dst_cv = a.dst[0].for_write('dst_img')
   print("dst_cv", dst_cv)
   z_offset = 1
   uncomposed_field_cv = a.dst[z_offset].for_read('field')
 
   vector_mip = args.mip
-  image_mip = 1
-  composed_range = z_range[3:]
+  image_mip = args.render_low_mip
+  composed_range = z_range
   for z in composed_range:
     a.render_to_low_mip(z, field_cv, z, dst_cv, z, bbox, image_mip, vector_mip)
 
