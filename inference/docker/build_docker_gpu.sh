5a3fa2c48d614af90374521ccb172fce705fe4eb
Thomas <tmacrina@princeton.edu>
2019-01-02 11:22:12 -0500

    Checkout docker files from multi-gpu-pairwise

---
+++ inference/docker/build_docker_gpu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+docker build -t seunglab/pairwise_inference:gpu_test -f Dockerfile.gpu ../../
+docker push seunglab/pairwise_inference:gpu_test
