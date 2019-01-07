b715fb5d150240a6b4d8371dd98ca56d9bce2272
Sergiy Popovych <sergiy.popovich@gmail.com>
2019-01-07 08:58:05 -0500

    cleanup

--- align_env/bin/client.py
+++
@@ -1,7 +0,0 @@
-#!/usr/people/popovych/SEAMLeSS/align_env/bin/python3
-# EASY-INSTALL-DEV-SCRIPT: 'seamless==0.0.1.dev574','client.py'
-__requires__ = 'seamless==0.0.1.dev574'
-__import__('pkg_resources').require('seamless==0.0.1.dev574')
-__file__ = '/usr/people/popovych/SEAMLeSS/inference/client.py'
-with open(__file__) as f:
-    exec(compile(f.read(), __file__, 'exec'))
