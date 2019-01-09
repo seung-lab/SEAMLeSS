504b97e3a6552c0c8f820f702e2f1004d982873a
Thomas Macrina <tmacrina@princeton.edu>
2019-01-09 17:54:20 -0500

    Update docker & kube files to accommodate setup.py

--- inference/docker/seamless.sh
+++ inference/docker/seamless.sh
@@ -1,7 +1,7 @@
-#! /bin/sh
+#!/bin/sh
 #install NVIDIA GPU device drivers
 kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/stable/nvidia-driver-installer/cos/daemonset-preloaded.yaml
 #create secrets for AWS and Google cloud
-kubectl create -f secret.yaml
-kubectl create secret generic secrets --from-file=/usr/people/zhenj/.cloudvolume/secrets/google-secret.json --from-file=/usr/people/zhenj/.cloudvolume/secrets/seunglab2-google-secret.json
-#kubectl create -f deploy.yaml
+kubectl create secret generic secrets --from-file=/usr/people/tmacrina/.cloudvolume/secrets/google-secret.json --from-file=/usr/people/tmacrina/.cloudvolume/secrets/seunglab2-google-secret.json
+kubectl create -f secret.yaml 
+kubectl create -f deploy.yaml
