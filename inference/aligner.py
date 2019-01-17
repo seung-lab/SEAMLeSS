1ffd1baf4cc7449f644a6ffeb81fbd2f7f8f45e0
jiazhentim <jiazhen1987@gmail.com>
2019-01-17 06:59:13 -0500

    fix bugs for distributed rechunking (#19)
    * fix bugs for distributed rechunking
    
    * cover cloudvolume gridsample code back
    
    * adjust cloudvolume gridsample function due to image rotated

--- inference/aligner.py
+++ inference/aligner.py
@@ -35,7 +35,8 @@ from task_handler import TaskHandler, make_residual_task_message, \
         make_downsample_task_message, make_compose_task_message, \
         make_prepare_task_message, make_vector_vote_task_message, \
         make_regularize_task_message, make_render_low_mip_task_message, \
-        make_invert_field_task_message, make_render_cv_task_message
+        make_invert_field_task_message, make_render_cv_task_message, \
+        make_batch_render_message
 
 class Aligner:
   """
@@ -108,6 +109,7 @@ class Aligner:
     # info = DstDir.create_info_batch(src_cv, mip_range, max_displacement, 2,
     #                                 256, self.process_low_mip)
     info = DstDir.create_info(src_cv, mip_range, max_displacement)
+    #info = src_cv.info
     self.dst = {}
     self.tgt_radius = tgt_radius
     self.tgt_range = range(-tgt_radius, tgt_radius+1)
@@ -634,28 +636,36 @@ class Aligner:
     cid = ((in_y_range[0] - calign_y_range[0]) // in_y_len) * line_bbox_num + (in_x_range[0] - calign_x_range[0]) // in_x_len
     return cid
 
+  def avg_field(self, field):
+      favg = field.sum() / torch.nonzero(field).size(0)
+      return favg
+
   def profile_field(self, field):
-      min_x = math.floor(np.min(field[...,0]))
-      min_y = math.floor(np.min(field[...,1]))
-      return np.float32([min_x, min_y])
+     # print(f.shape)
+     # min_x = math.floor(np.min(field[...,0]))
+     # min_y = math.floor(np.min(field[...,1]))
+     # return np.float32([min_x, min_y])
+      avg_x = self.avg_field(field[0,...,0])
+      avg_y = self.avg_field(field[0,...,1])
+      return torch.from_numpy(np.float32([avg_x, avg_y]))
 
   def adjust_bbox(self, bbox, dis):
       influence_bbox = deepcopy(bbox)
       x_range = influence_bbox.x_range(mip=0)
       y_range = influence_bbox.y_range(mip=0)
       #print("x_range is", x_range, "y_range is", y_range)
-      new_bbox = BoundingBox(x_range[0] - dis[0], x_range[1] - dis[0],
-                                   y_range[0] - dis[1], y_range[1] - dis[1],
+      new_bbox = BoundingBox(x_range[0] + dis[1], x_range[1] + dis[1],
+                                   y_range[0] + dis[0], y_range[1] + dis[0],
                                    mip=0)
       #print(new_bbox.x_range(mip=0), new_bbox.y_range(mip=0))
       return new_bbox
 
   def gridsample_cv(self, image_cv, field_cv, bbox, z, mip):
       f =  self.get_field(field_cv, z, bbox, mip, relative=False,
-                          to_tensor=True) 
+                          to_tensor=True)
       x_range = bbox.x_range(mip=0)
       y_range = bbox.y_range(mip=0)
-      if torch.min(field) == 0 and torch.max(field) == 0:
+      if torch.min(f) == 0 and torch.max(f) == 0:
           image = self.get_image(image_cv, z, bbox, mip,
                                  adjust_contrast=False, to_tensor=True)
           return image
@@ -673,7 +683,6 @@ class Aligner:
           f = f - distance.to(device = self.device)
           #f = f - distance
           res = self.abs_to_rel_residual(f, bbox, mip)
-          #res = torch.from_numpy(res)
           field = res.to(device = self.device)
           #print("field shape is", field.shape)
           image = self.get_image(image_cv, z, new_bbox, mip,
@@ -1011,7 +1020,7 @@ class Aligner:
                 task_patches.append(chunks[j])
             render_task_batch = make_batch_render_message(src_z, field_cv, field_z, task_patches,
                                                    mip, dst_cv, dst_z, batch)
-            self.task_handler.send_message(render_task)
+            self.task_handler.send_message(render_task_batch)
         self.task_handler.wait_until_ready()
     else:
         def chunkwise(patch_bbox):
