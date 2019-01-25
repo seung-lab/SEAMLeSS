410b3e5403ac54122d76cde8e6a2164e50270bf0
William Silversmith <william.silversmith@gmail.com>
2019-01-25 18:29:01 -0500

    wip: converting to using TaskQueue

--- inference/worker.py
+++ inference/worker.py
@@ -1,11 +1,16 @@
 import sys
 import torch
 from args import get_argparser, parse_args, get_aligner, get_bbox 
-from os.path import join
+import task_handler # required to initialize RegisteredTasks
+
+from taskqueue import TaskQueue
 
 if __name__ == '__main__':
   parser = get_argparser()
   args = parse_args(parser) 
-  a = get_aligner(args)
+  aligner = get_aligner(args)
 
-  a.listen_for_tasks()
+  with TaskQueue(queue_name=args['queue_name'], queue_server='sqs', n_threads=0) as tq:
+    tq.poll(execute_args=[ aligner ])
+
+
