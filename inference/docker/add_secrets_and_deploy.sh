8e439cd0ca69503e918589ec2d4aa7c539d78f54
Nico Kemnitz <nkemnitz@princeton.edu>
2019-01-27 15:01:32 -0500

    Upgrade to CUDA 10 (#22)
    * remove obsolete cloudvolume hack
    
    * Upgrade to CUDA 10 for Turing GPUs

--- inference/docker/add_secrets_and_deploy.sh
+++ inference/docker/add_secrets_and_deploy.sh
@@ -2,10 +2,10 @@
 # connect to cluster
 gcloud container clusters get-credentials $1 --zone us-central1-a --project iarpa-microns-seunglab
 #install NVIDIA GPU device drivers
-kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/stable/nvidia-driver-installer/cos/daemonset-preloaded.yaml
+kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/master/daemonset.yaml
 #create secrets for AWS and Google cloud
-kubectl create secret generic secrets --from-file=/usr/people/tmacrina/.cloudvolume/secrets/google-secret.json --from-file=/usr/people/tmacrina/.cloudvolume/secrets/seunglab2-google-secret.json --from-file=/usr/people/tmacrina/.cloudvolume/secrets/microns-seunglab-google-secret.json
-kubectl create -f secret.yaml 
+kubectl create secret generic secrets --from-file=$HOME/.cloudvolume/secrets/google-secret.json --from-file=$HOME/.cloudvolume/secrets/seunglab2-google-secret.json --from-file=$HOME/.cloudvolume/secrets/microns-seunglab-google-secret.json
+#kubectl create -f secret.yaml 
 kubectl create -f deploy.yaml
 # scale deployment 
 # kubectl scale deployment seamless --replicas=64
