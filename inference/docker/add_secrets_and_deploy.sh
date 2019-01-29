c9d80c52e025ef91d8ebe6e4e7f1a669e84ac87c
Thomas Macrina <tmacrina@princeton.edu>
2019-01-29 15:56:59 -0500

    Update add secrets

--- inference/docker/add_secrets_and_deploy.sh
+++ inference/docker/add_secrets_and_deploy.sh
@@ -4,8 +4,7 @@ gcloud container clusters get-credentials $1 --zone us-central1-a --project iarp
 #install NVIDIA GPU device drivers
 kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/stable/nvidia-driver-installer/cos/daemonset-preloaded.yaml
 #create secrets for AWS and Google cloud
-kubectl create secret generic secrets --from-file=$HOME/.cloudvolume/secrets/google-secret.json --from-file=$HOME/.cloudvolume/secrets/seunglab2-google-secret.json --from-file=$HOME/.cloudvolume/secrets/microns-seunglab-google-secret.json
-kubectl create -f secret.yaml 
+kubectl create secret generic secrets --from-file=$HOME/.cloudvolume/secrets/google-secret.json --from-file=$HOME/.cloudvolume/secrets/seunglab2-google-secret.json --from-file=$HOME/.cloudvolume/secrets/microns-seunglab-google-secret.json --from-file=$HOME/.cloudvolume/secrets/aws-secret.json
 kubectl create -f deploy.yaml
 # scale deployment 
 # kubectl scale deployment seamless --replicas=64
