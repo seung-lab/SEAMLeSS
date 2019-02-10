27a4e09f6e45914c6e0bf3617d44484008984712
supersergiy <sergiy.popovich@gmail.com>
2019-02-10 12:38:41 -0500

    m

--- models/create_model.py
+++ models/create_model.py
@@ -9,7 +9,8 @@ parser.add_argument('--weights_dir', type=str, default='/usr/people/popovych/ali
 parser.add_argument('--template', type=str, default="sergiy_m8m10_template")
 parser.add_argument('--name_template', type=str, default="zzzz_02_03_mip_10_z{}")
 parser.add_argument('--prefix', type=str, default="sergiy_m8m10_")
-parser.add_argument('--mips', type=list, default=[8, 10])
+parser.add_argument('--mips_trained', type=list, default=[10])
+parser.add_argument('--mips_copied', type=list, default=[])
 parser.add_argument('--section', type=int, default=None)
 args = parser.parse_args()
 
@@ -25,17 +26,18 @@ for z in starts:
     net_dir = "{}{}".format(args.prefix, name)
     src_weights_dir = args.weights_dir
     tgt_weights_dir = os.path.join(net_dir, "weights.pt")
+    print("cp -r {} {}".format(args.template, net_dir))
     subprocess.Popen("cp -r {} {}".format(args.template, net_dir), shell=True)
 
 
-    for m in [10]:
+    for m in args.mips_trained:
         file_name = "{}_module{}.pth.tar".format(name, m)
         file_path = os.path.join(src_weights_dir, file_name)
         tgt_path = os.path.join(tgt_weights_dir, "module{}.pth.tar".format(m))
         print ("cp {} {}".format(file_path, tgt_weights_dir))
         subprocess.Popen("cp {} {}".format(file_path, tgt_path), shell=True)
 
-    for m in [8]:
+    for m in args.mips_copied:
         file_name = "serial_x1_module{}.pth.tar".format(m)
         file_path = os.path.join(src_weights_dir, file_name)
         tgt_path = os.path.join(tgt_weights_dir, "module{}.pth.tar".format(m))
