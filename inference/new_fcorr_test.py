b6cad36fe16e475f8c9c707423110ac0d8b658ee
Zhen Jia <zhenj@seungworkstation1002.princeton.edu>
2019-03-01 11:32:15 -0500

    using new task queue

--- inference/new_fcorr_test.py
+++ inference/new_fcorr_test.py
@@ -3,7 +3,7 @@ gevent.monkey.patch_all()
 
 from concurrent.futures import ProcessPoolExecutor
 import taskqueue
-from taskqueue import TaskQueue, GreenTaskQueue, LocalTaskQueue
+from taskqueue import TaskQueue, GreenTaskQueue
 
 import sys
 import torch
@@ -36,8 +36,13 @@ if __name__ == '__main__':
   parser = get_argparser()
   parser.add_argument('--model_path', type=str,
     help='relative path to the ModelArchive to use for computing fields')
-  parser.add_argument('--src_path1', type=str)
-  parser.add_argument('--src_path2', type=str)
+  parser.add_argument('--src_path', type=str)
+  parser.add_argument('--src_mask_path', type=str, default='',
+    help='CloudVolume path of mask to use with src images; default None')
+  parser.add_argument('--src_mask_mip', type=int, default=8,
+    help='MIP of source mask')
+  parser.add_argument('--src_mask_val', type=int, default=1,
+    help='Value of of mask that indicates DO NOT mask')
   parser.add_argument('--dst_path', type=str)
   parser.add_argument('--mip', type=int)
   parser.add_argument('--bbox_start', nargs=3, type=int,
@@ -66,29 +71,32 @@ if __name__ == '__main__':
   # Compile ranges
   full_range = range(args.bbox_start[2], args.bbox_stop[2])
   # Create CloudVolume Manager
-  cm = CloudManager(args.src_path1, max_mip, pad, provenance, batch_size=1,
-                    size_chunk=128, batch_mip=mip) 
-  #cm = CloudManager(args.src_path1, max_mip, pad, provenance)
+  cm = CloudManager(args.src_path, max_mip, pad, provenance, batch_size=1,
+                    size_chunk=128, batch_mip=mip+3)
+  #cm = CloudManager(args.src_path, max_mip, pad, provenance)
 
   # Create src CloudVolumes
-  src1 = cm.create(args.src_path1, data_type='float', num_channels=1,
-                     fill_missing=True, overwrite=False)
-
-  src2 = cm.create(args.src_path2, data_type='float', num_channels=1,
+  src = cm.create(args.src_path, data_type='uint8', num_channels=1,
                      fill_missing=True, overwrite=False)
 
   # Create dst CloudVolumes
   dst = cm.create(join(args.dst_path, 'image'),
                   data_type='float32', num_channels=1, fill_missing=True,
                   overwrite=True)
+  dst1 = cm.create(join(args.dst_path, 'image_nopost'),
+                  data_type='float32', num_channels=1, fill_missing=True,
+                  overwrite=True)
+
   prefix = str(mip)
   class TaskIterator():
       def __init__(self, brange):
           self.brange = brange
       def __iter__(self):
           for z in self.brange:
-              t = a.mask_op(cm, bbox, mip, z, z, src1, src2, dst, z)
-              yield from t
+            #print("Fcorr for z={} and z={}".format(z, z+1))
+            t = a.calculate_fcorr(cm, bbox, mip, z, z+2, src, dst,
+                                  dst1, prefix)
+            yield from t
 
   range_list = make_range(full_range, a.threads)
 
@@ -101,8 +109,6 @@ if __name__ == '__main__':
   for i in range_list:
       ptask.append(TaskIterator(i))
 
-  #with  LocalTaskQueue(parallel=1) as tq:
-  #    tq.insert_all(ptask[0], args=[a])
   with ProcessPoolExecutor(max_workers=a.threads) as executor:
       executor.map(remote_upload, ptask)
 
