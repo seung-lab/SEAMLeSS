f8b98fc77159999742854bc8c421c076f4647b56
jabae@princeton.edu <jabae@princeton.edu>
2019-03-08 02:23:27 -0500

    all black chunk bug fixed

--- inference/fold_detec_post.py
+++ inference/fold_detec_post.py
@@ -51,7 +51,7 @@ def filter_mask(img, size_thr):
 	img_lab = measure.label(img)
 
 	fold_num, fold_size = np.unique(img_lab, return_counts=True)
-	if fold_num.shape[0] != 1:
+	if fold_num.shape[0] > 1 or np.sum(fold_num) == 0:
 		fold_num = fold_num[1:]; fold_size = fold_size[1:]
 
 	img_lab_vec = np.reshape(img_lab, (-1,))
