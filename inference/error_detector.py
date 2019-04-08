fd6b7529e1c8836deb6dc8ee8cedfa272fafea0a
jabae@princeton.edu <jabae@princeton.edu>
2019-04-07 23:30:49 -0400

    Update error_detector.py

--- inference/error_detector.py
+++ inference/error_detector.py
@@ -135,8 +135,8 @@ def inference(model, seg, img, patch_size):
 
 		pred = torch.sigmoid(model(input_patch))
 		pred_reshape = torch.reshape(pred, (1,)+pred.shape[2:]) 
-		pred_upsample = F.interpolate(pred_reshape, scale_factor=16, mode="nearest").cpu().detach().numpy()
-		error_vol[focus] = np.maximum(error_vol[focus], pred_upsample*obj_patch).astype('uint8')
+		pred_upsample = F.interpolate(pred_reshape, scale_factor=16, mode="nearest").cpu().detach()
+		error_vol[focus] = np.maximum(error_vol[focus], pred_upsample*obj_patch).numpy().astype('uint8')
 
 		vis_vol[focus] = torch.from_numpy(vis_vol[focus]) + obj_patch[:,:,8:24,80:240,80:240]
 
