83c9f0f0371ba86154e5822edd12e2e93830e943
Thomas Macrina <tmacrina@princeton.edu>
2019-04-24 14:40:44 -0400

    Update cpc script to follow latest convention

--- inference/cpc.py
+++ inference/cpc.py
@@ -1,3 +1,10 @@
+import gevent.monkey
+gevent.monkey.patch_all()
+
+from concurrent.futures import ProcessPoolExecutor
+import taskqueue
+from taskqueue import TaskQueue, GreenTaskQueue, LocalTaskQueue
+
 import sys
 import torch
 import json
@@ -9,6 +16,20 @@ from tasks import run
 
 def print_run(diff, n_tasks):
   print (": {:.3f} s, {} tasks, {:.3f} s/tasks".format(diff, n_tasks, diff / n_tasks))
+
+def make_range(block_range, part_num):
+    rangelen = len(block_range)
+    if(rangelen < part_num):
+        srange =1
+        part = rangelen
+    else:
+        part = part_num
+        srange = rangelen//part
+    range_list = []
+    for i in range(part-1):
+        range_list.append(block_range[i*srange:(i+1)*srange])
+    range_list.append(block_range[(part-1)*srange:])
+    return range_list
 
 if __name__ == '__main__':
   parser = get_argparser()
@@ -57,27 +78,49 @@ if __name__ == '__main__':
     data_type = 'float32'
   
   # Create dst CloudVolumes for each block, since blocks will overlap by 3 sections
-  dst = cm.create(join(args.src_path, 'cpc', '{}_{}'.format(args.src_mip, args.dst_mip),
-                       '{}'.format(args.z_offset)), 
-                  data_type=data_type, num_channels=1, fill_missing=True, 
+  dst_path = join(args.src_path, 'cpc', '{}_{}'.format(args.src_mip, args.dst_mip),
+                  '{}'.format(args.z_offset)) 
+  if args.unnormalized:
+    dst_path = join(args.src_path, 'cpc', 'unnormalized', 
+                    '{}_{}'.format(args.src_mip, args.dst_mip),
+                    '{}'.format(args.z_offset)) 
+  dst = cm.create(dst_path, data_type=data_type, num_channels=1, fill_missing=True, 
                   overwrite=True).path
 
   ##############
   # CPC script #
   ##############
-  k = 0
-  batch = []
-  for z in z_range:
-    t = a.cpc(cm, src, src, dst, z, z+args.z_offset, bbox, 
-                  args.src_mip, args.dst_mip, norm=not args.unnormalized)
-    batch.extend(t)
-    k += 1
-    if k >= 100:
-      print('Scheduling CPC for {} tasks'.format(len(batch)))
-      run(a, batch)
-      batch = []
-      k = 0
+  class TaskIterator():
+      def __init__(self, brange):
+          self.brange = brange
+      def __iter__(self):
+          for z in self.brange:
+            #print("Fcorr for z={} and z={}".format(z, z+1))
+            t = a.cpc(cm, src, src, dst, z, z+args.z_offset, bbox, 
+                      args.src_mip, args.dst_mip, norm=not args.unnormalized)
+            yield from t
 
-  run(a, batch)
-  print('Finished scheduling. Watch queue for finish.')
+  range_list = make_range(z_range, a.threads)
 
+  def remote_upload(tasks):
+    with GreenTaskQueue(queue_name=args.queue_name) as tq:
+        tq.insert_all(tasks)
+
+  start = time()
+  ptask = []
+  for i in range_list:
+      ptask.append(TaskIterator(i))
+
+  if a.distributed:
+    with ProcessPoolExecutor(max_workers=a.threads) as executor:
+        executor.map(remote_upload, ptask)
+  else:
+      for t in ptask:
+        tq = LocalTaskQueue(parallel=1)
+        tq.insert_all(t, args= [a])
+
+
+  end = time()
+  diff = end - start
+  print("Sending Tasks use time:", diff)
+  print('Running Tasks')
