6cfa7004127735e1c21079e2854a553f448106ec
jabae <jabae@seungworkstation18.Princeton.EDU>
2019-05-19 14:58:18 -0400

    chunk volume

--- inference/error_detector.py
+++ inference/error_detector.py
@@ -2,6 +2,9 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
+import itertools
+import operator
+
 
 # Volume format convenient to extract patch.
 class Volume():
@@ -168,6 +171,41 @@ def visited_init(seg, volume_size, patch_size):
 		
 # 	return error_vol.A
 
+
+def bounds1D(start, end, step_size, overlap=0):
+  
+  assert step_size > 0, "Invalid step_size: {}".format(step_size)
+  assert end > start, "Invalid range: {} ~ {}".format(start, end)
+  assert overlap >=0, "Invalid overlap: {}".format(overlap)
+
+  s = start
+  e = s + step_size
+
+  bounds = []
+  while e < end:
+    
+    bounds.append((s, e))
+
+    s += step_size - overlap
+    e = s + step_size
+
+  e = end
+  bounds.append((s,e))
+
+  return bounds
+
+
+def chunk_bboxes(bbox_start, bbox_end, chunk_size, overlap=(0,0,0)):
+
+	x_bnds = bounds1D(bbox_start[0], bbox_end[0], chunk_size[0], overlap[0])
+	y_bnds = bounds1D(bbox_start[1], bbox_end[1], chunk_size[1], overlap[1])
+	z_bnds = bounds1D(bbox_start[2], bbox_end[2], chunk_size[2], overlap[2])
+
+	bboxes = [tuple(zip(xs, ys, zs))
+						for (xs, ys, zs) in itertools.product(x_bnds, y_bnds, z_bnds)]
+
+	return bboxes
+	
 
 def sample_objects_chunked(vol_seg, volume_size, patch_size, visited_size, chunk_size, mip=0):
 
