7379edc57659267865b1dfe4efdb6aeeebbccebb
Sergiy Popovych <sergiy.popovich@gmail.com>
2019-11-02 19:22:59 -0700

    change igneous ranges

---
+++ inference/igneous_downsample_folds.py
@@ -0,0 +1,18 @@
+import sys
+from taskqueue import TaskQueue
+import igneous.task_creation as tc
+from cloudvolume import Bbox
+
+with TaskQueue('deepalign-igneous-1') as tq:
+    tasks = tc.create_downsampling_tasks(
+        'precomputed://gs://seunglab_minnie_phase3/alignment/precoarse_vv5_tempdiv4_step128_maxdisp128/warped_folds_final',
+        chunk_size=[1024, 1024, 1],
+        fill_missing=True,
+        bounds=Bbox((-1312 + 2048, -1312 + 2048, 17000), (524288, 393216, 18000)),
+        mip=4,
+        num_mips=4,
+        delete_black_uploads=True,
+        sparse=True
+    )
+    tq.insert_all(tasks)
+print("Done!")
