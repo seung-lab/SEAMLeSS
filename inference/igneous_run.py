44a294665d2cc34eb59373bf55c1962e2380678f
Sergiy Popovych <sergiy.popovich@gmail.com>
2019-11-12 01:49:55 -0800

    latest model

--- inference/igneous_run.py
+++ inference/igneous_run.py
@@ -6,12 +6,12 @@ from cloudvolume import Bbox
 with TaskQueue('deepalign-igneous-1') as tq:
 
   tasks = tc.create_downsampling_tasks(
-    'precomputed://gs://seunglab_minnie_phase3/alignment/precoarse_vv5_tempdiv4_step128_maxdisp128/warped_result',
+    'precomputed://gs://seunglab_minnie_phase3/alignment/fine_inference_x2_optim300_lr3em2_sm25e1_maskthresh07_mse3_vv5_long/warped_em_fromcoarse',
     chunk_size=[1024, 1024, 1],
     fill_missing=True,
-    bounds=Bbox((0, 0, 14780), (524288, 393216, 27883)),
-    mip=4,
-    num_mips=4,
+    bounds=Bbox((0, 0, 17405), (524288, 393216, 17600)),
+    mip=2,
+    num_mips=6,
     preserve_chunk_size=True,
     delete_black_uploads=True
 )
