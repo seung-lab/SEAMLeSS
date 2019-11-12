8ea22285f090b192c237258b93f13c6393270db5
Thomas Macrina <tmacrina@princeton.edu>
2019-11-12 11:44:41 -0500

    Add back multiGPU training

--- utilities/parallel.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-class DataParallelAccessAttributes(torch.nn.DataParallel):
-    """Subclass to access attributes of the wrapped Module
-    """
-    def __getattr__(self, name):
-        return getattr(self.module, name)
