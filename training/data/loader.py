7caa225b3a395583ed9c302a6d90fdd03cb77d10
Thomas Macrina <tmacrina@princeton.edu>
2019-11-25 16:18:29 -0500

    Extract DataLoader creation to data/loader.py

---
+++ training/data/loader.py
@@ -0,0 +1,64 @@
+import torch.utils.data
+import torchvision.transforms as transforms
+import stack_dataset
+
+class DataLoader(object):
+    """Template for training & validation data loaders below
+    """
+    def __init__(self, args):
+        self.transform = None
+        self.dataset = None
+        self.sampler = None
+        self.loader = None
+
+class TrainingDataLoader(DataLoader):
+    """Object to manage the DataLoader for training samples:
+    """
+    def __init__(self, state_vars, archive):
+        self.transform = transforms.Compose([
+                stack_dataset.ToFloatTensor(),
+                stack_dataset.Preprocess(archive.preprocessor),
+                stack_dataset.OnlyIf(stack_dataset.RandomRotateAndScale(),
+                                     not state_vars.skip_aug),
+                stack_dataset.OnlyIf(stack_dataset.RandomFlip(),
+                                     not state_vars.skip_aug),
+                stack_dataset.Split(),
+                stack_dataset.OnlyIf(stack_dataset.RandomTranslation(20),
+                                     not state_vars.skip_aug),
+                stack_dataset.OnlyIf(stack_dataset.RandomField(),
+                                     state_vars.supervised),
+                stack_dataset.OnlyIf(stack_dataset.RandomAugmentation(),
+                                     not state_vars.skip_aug),
+                stack_dataset.ToDevice('cpu'),
+        ])
+        self.dataset = stack_dataset.compile_dataset(
+                            state_vars.training_set_path,
+                            transform=self.transform,
+                            num_samples=state_vars.num_samples,
+                            repeats=state_vars.repeats)
+        self.sampler = torch.utils.data.RandomSampler(self.dataset)
+        self.loader = torch.utils.data.DataLoader(
+                        self.dataset,
+                        batch_size=state_vars.batch_size,
+                        shuffle=(self.sampler is None),
+                        num_workers=state_vars.num_workers,
+                        pin_memory=(state_vars.validation_set_path is None),
+                        sampler=self.sampler)
+
+class ValidationDataLoader(DataLoader):
+
+    def __init__(self, state_vars, archive):
+        self.transform = transforms.Compose([
+                            stack_dataset.ToFloatTensor(),
+                            stack_dataset.Preprocess(archive.preprocessor),
+                            stack_dataset.Split(),
+        ])
+        self.dataset = stack_dataset.compile_dataset(
+                            state_vars.validation_set_path,
+                            transform=self.transform)
+        self.loader = torch.utils.data.DataLoader(
+                        self.dataset,
+                        batch_size=1,
+                        shuffle=False,
+                        num_workers=0,
+                        pin_memory=False)
