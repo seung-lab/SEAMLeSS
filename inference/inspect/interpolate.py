b3872c2ae08f9c428cc612c3c39e030d2adbd936
Thomas Macrina <tmacrina@princeton.edu>
2020-01-22 18:39:51 -0500

    Write to field CloudVolume from interpolate.py

--- inference/inspect/interpolate.py
+++ inference/inspect/interpolate.py
@@ -2,6 +2,12 @@ import argparse
 from scipy.interpolate import griddata
 import numpy as np
 import pandas as pd
+from getpass import getuser
+from args import get_bbox
+from aligner import Aligner
+from cloudmanager import CloudManager
+from mipless_cloudvolume import MiplessCloudVolume
+from boundingbox import BoundingBox
 
 class SubsampledField():
 
@@ -53,12 +59,9 @@ def grid_to_points(grid_v, grid_x, grid_y):
             data.append([x,y,x+v[0],y+v[1]])
     return data 
 
-def make_grid(x_start, x_stop, y_start, y_stop, mip=0):
-    s = 2**mip
-    x_start = int(np.floor(x_start/s))
-    y_start = int(np.floor(y_start/s))
-    x_stop = int(np.ceil(x_stop/s))
-    y_stop = int(np.ceil(y_stop/s))
+def make_grid(x_start, x_stop, y_start, y_stop):
+    """Args should be adjusted to MIP level
+    """
     return np.mgrid[x_start:x_stop, y_start:y_stop]
     
 def add_z(points, z0, z1):
@@ -82,6 +85,7 @@ def test_subsample():
                        columns=['x0','y0','x1','y1'])
     idf = idf * s 
     isf = SubsampledField(idf)
+    # TODO: assert() # verified this manually
 
 if __name__ == '__main__':
     # test_subsample()
@@ -95,6 +99,14 @@ if __name__ == '__main__':
         type=str,
         help='path of where to save interpolated grid') 
     parser.add_argument(
+        '--info_path',
+        type=str,
+        help='path to CloudVolume to use as template for field CloudVolume') 
+    parser.add_argument(
+        '--field_path',
+        type=str,
+        help='path to CloudVolume where to save interpolated grid') 
+    parser.add_argument(
         '--method',
         type=str,
         default='cubic',
@@ -104,19 +116,48 @@ if __name__ == '__main__':
         type=int,
         help='MIP level of interpolated grid')
     parser.add_argument(
-        '--bbox',
-        nargs=6,
+        '--src_z',
         type=int,
-        help='interpolated grid bound (x_start, x_stop, y_start, y_stop, z_start, z_stop)')
+        help='z index of source section')
+    parser.add_argument(
+        '--tgt_z',
+        type=int,
+        help='z index of target section')
     args = parser.parse_args()
     src_df = pd.read_csv(args.src_path, index_col=0)
+    provenance = {'description': 'Field created from manual correspondences & interpolation',
+                  'sources': [args.info_path],
+                  'owners': [getuser()],
+                  'processing': [vars(args)]}
+    cm = CloudManager(cv_path=args.info_path, 
+                      max_mip=args.mip, 
+                      max_displacement=0,
+                      provenance=provenance, 
+                      create_info=True)
+    dst = cm.create(path=args.field_path, 
+                      data_type='float32', 
+                      num_channels=2,
+                      fill_missing=True, 
+                      overwrite=True)
+    bbox = BoundingBox.from_bbox(dst[args.mip].bounds, mip=args.mip, max_mip=args.mip)
     f = SubsampledField(src_df)
     f.compute_delta()
-    grid_x, grid_y = make_grid(*args.bbox[:4], mip=args.mip)
+    grid_x, grid_y = make_grid(*bbox.x_range(mip=args.mip), 
+                               *bbox.y_range(mip=args.mip))
     grid_v = f.interpolate(grid_x, grid_y, mip=args.mip, method=args.method)
+    field = grid_v.astype(np.float32) * 2**args.mip
+    field = -np.expand_dims(field, axis=0)[:,:,:,::-1]
+    a = Aligner()
+    a.save_field(field=field,
+                 cv=dst,
+                 z=args.src_z,
+                 bbox=bbox,
+                 mip=args.mip,
+                 relative=False,
+                 as_int16=False)
     points = grid_to_points(grid_v, grid_x, grid_y)
     df = pd.DataFrame(data=np.array(points)*2**args.mip,
                        columns=['x0','y0','x1','y1'])
-    df['z0'] = args.bbox[4]
-    df['z1'] = args.bbox[5]
+    df['z0'] = args.src_z
+    df['z1'] = args.tgt_z
     df.to_csv(args.dst_path, header=True, index=True)
