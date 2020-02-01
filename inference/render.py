a3cdd63137048cfaa8a718540eaa74be22d0d1c9
Thomas Macrina <tmacrina@princeton.edu>
2020-01-31 19:45:30 -0500

    Remove monkey patch in render.py for LocalTaskQueue in method instead of main

--- inference/render.py
+++ inference/render.py
@@ -1,5 +1,5 @@
-import gevent.monkey
-gevent.monkey.patch_all(thread=False)
+# import gevent.monkey
+# gevent.monkey.patch_all(thread=False)
 
 from concurrent.futures import ProcessPoolExecutor
 import taskqueue
