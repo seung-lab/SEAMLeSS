d9199652eda136556d77c5252108468485cfbd0b
Thomas Macrina <tmacrina@princeton.edu>
2020-01-31 19:42:53 -0500

    Refactor features.py to create method out of main

--- inference/features.py
+++ inference/features.py
@@ -5,6 +5,60 @@ from cloudvolume import CloudVolume
 from cloudvolume.lib import Bbox, Vec
 import pandas as pd
 
+def feature_match(cv, bbox, src_z, tgt_z, mip, max_distance, min_hessian):
+    """Return matched SURF keypoints
+
+    Args:
+        cv: CloudVolume with images
+        bbox: Bbox for xy extent
+        src_z: int for section that will be warped
+        tgt_z: int for section to compare against
+        mip: int for MIP level of images
+        max_distance: number of max distance between keypoints
+        min_hessian: number for minimum of hessian determinant in SURF
+    
+    Returns
+        pd.DataFrame with correpsondences as x,y,z pairs
+    """
+    bslices = bbox.to_slices()
+    src = cv[bslices[0], bslices[1], src_z][:,:,0,0].T
+    tgt = cv[bslices[0], bslices[1], tgt_z][:,:,0,0].T
+
+    print('Detecting SURF keypoints')
+    detector = cv2.xfeatures2d_SURF.create(hessianThreshold=min_hessian)
+    keypoints_src, descriptors_src = detector.detectAndCompute(src, None)
+    keypoints_tgt, descriptors_tgt = detector.detectAndCompute(tgt, None)
+    print('Found {} keypoints in src'.format(len(keypoints_src)))
+    print('Found {} keypoints in tgt'.format(len(keypoints_tgt)))
+
+    bf = cv2.BFMatcher()
+    # matches = bf.match(descriptors_src, descriptors_tgt)
+    # matches = bf.radiusMatch(descriptors_src, 
+    #                               descriptors_tgt,
+    #                               maxDistance=max_distance/2**mip)
+    matches = bf.knnMatch(descriptors_src, descriptors_tgt, k=2)
+    print('Found {} matches within radius {}'.format(len(matches), max_distance / 2**mip))
+    points = []
+    for m,n in matches:
+        if (m.distance < 0.75*n.distance): 
+            src_kp = keypoints_src[m.queryIdx]
+            tgt_kp = keypoints_tgt[m.trainIdx]
+            src_pt = np.array(src_kp.pt) 
+            tgt_pt = np.array(tgt_kp.pt) 
+            if np.linalg.norm(tgt_pt - src_pt) < max_distance / 2**mip:
+                points.append([*src_kp.pt, *tgt_kp.pt])
+    cols = ['x0','y0','x1','y1']
+    if len(points) > 0:
+        df = pd.DataFrame(data=np.array(points)*2**mip,
+                          columns=cols)
+        df.loc[:,['x0','x1']] += bbox.minpt[0] * 2**mip
+        df.loc[:,['y0','y1']] += bbox.minpt[1] * 2**mip
+        df['z0'] = src_z
+        df['z1'] = tgt_z
+    else:
+        df = pd.DataFrame(columns=cols)
+    return df
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -53,37 +107,11 @@ if __name__ == '__main__':
     if args.bbox_start is not None:
         bbox = Bbox(Vec(*args.bbox_start), Vec(*args.bbox_stop))
         bbox = cv.bbox_to_mip(bbox, mip=0, to_mip=args.mip)
-    bslices = bbox.to_slices()
-    src = cv[bslices[0], bslices[1], args.src_z][:,:,0,0].T
-    tgt = cv[bslices[0], bslices[1], args.tgt_z][:,:,0,0].T
-
-    print('Detecting SURF keypoints')
-    detector = cv2.xfeatures2d_SURF.create(hessianThreshold=args.min_hessian)
-    keypoints_src, descriptors_src = detector.detectAndCompute(src, None)
-    keypoints_tgt, descriptors_tgt = detector.detectAndCompute(tgt, None)
-    print('Found {} keypoints in src'.format(len(keypoints_src)))
-    print('Found {} keypoints in tgt'.format(len(keypoints_tgt)))
-
-    bf = cv2.BFMatcher()
-    # matches = bf.match(descriptors_src, descriptors_tgt)
-    # matches = bf.radiusMatch(descriptors_src, 
-    #                               descriptors_tgt,
-    #                               maxDistance=args.max_distance/2**args.mip)
-    matches = bf.knnMatch(descriptors_src, descriptors_tgt, k=2)
-    print('Found {} matches within radius {}'.format(len(matches), args.max_distance / 2**args.mip))
-    points = []
-    for m,n in matches:
-        if (m.distance < 0.75*n.distance): 
-            src_kp = keypoints_src[m.queryIdx]
-            tgt_kp = keypoints_tgt[m.trainIdx]
-            src_pt = np.array(src_kp.pt) 
-            tgt_pt = np.array(tgt_kp.pt) 
-            if np.linalg.norm(tgt_pt - src_pt) < args.max_distance / 2**args.mip:
-                points.append([*src_kp.pt, *tgt_kp.pt])
-    df = pd.DataFrame(data=np.array(points)*2**args.mip,
-                      columns=['x0','y0','x1','y1'])
-    df.loc[:,['x0','x1']] += bbox.minpt[0] * 2**args.mip
-    df.loc[:,['y0','y1']] += bbox.minpt[1] * 2**args.mip
-    df['z0'] = args.src_z
-    df['z1'] = args.tgt_z
+    df = feature_match(cv=cv, 
+                        bbox=bbox, 
+                        src_z=args.src_z, 
+                        tgt_z=args.tgt_z, 
+                        mip=args.mip, 
+                        max_distance=args.max_distance, 
+                        min_hessian=args.min_hessian)
     df.to_csv(args.points_path, header=True, index=True)
