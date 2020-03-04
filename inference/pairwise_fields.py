bf5d0f876247e7a1053100af40a5c5558b7867e1
Thomas Macrina <tmacrina@princeton.edu>
2020-03-04 22:59:10 +0000

    Add PairwiseVoteTask

--- inference/pairwise_fields.py
+++ inference/pairwise_fields.py
@@ -1,9 +1,15 @@
 import os
+from time import time
+
+import torch
 import numpy as np
+
 from boundingbox import BoundingBox, BoundingCube
 from mipless_cloudvolume import MiplessCloudVolume
 from fields import FieldCloudVolume 
 from cloudsample import cloudsample_multicompose
+
+from taskqueue import RegisteredTask
 
 class PairwiseFields():
 
@@ -104,4 +110,105 @@ class PairwiseFields():
                                             factors=None,
                                             pad=self.pad)
         
+
+class PairwiseVoteTask(RegisteredTask):
+    def __init__(self, estimates_path, corrected_path, weights_path, 
+                        offsets, src_z, tgt_offsets, bbox, mip, pad,
+                        as_int16, device, softmin_temp, blur_sigma):
+        """Correct all pairwise fields from src_z to src_z+tgt_offsets
+
+        Args:
+            estimates_path: path to directory with pairwise fields to
+                be corrected.
+            corrected_path: path to directory where corrected pairwise
+                field will be stored
+            weights_path: path to directory where weights of corrected
+                pairwise field will be stored
+            offsets: list of ints indicating which offset subdirectories
+                are available for the PairwiseField to use
+            src_z: int for z index of section which will always be the 
+                source in all pairwise fields
+            tgt_offsets: list of ints indicating set of offsets to
+                be used as tgts for pairwise fields; must be subset of
+                `offsets`.
+            bbox: serialized BoundingBox
+            mip: int for MIP level of all fields
+            softmine_temp: float for temperature of voting's softmin
+            blur_sigma: float for std of spatial Gaussian used before voting
+            pad: int used to uncrop field for profiling ahead of composition
+            as_int16: bool indicating if fields are stored in fixed-point
+            device: str indicating device for torch.Tensors
+        """
+        super().__init__(estimates_path, corrected_path, weights_path,
+                        offsets, src_z, tgt_offsets, bbox, mip, pad,
+                        as_int16, device, softmin_temp, blur_sigma)
+
+    def execute(self):
+        z = self.src_z
+        bbox = BoundingBox.deserialize(self.bbox)
+        estimated_fields = PairwiseFields(path=self.estimates_path, 
+                                         offsets=self.offsets,
+                                         bbox=bbox,
+                                         mip=self.mip,
+                                         pad=self.pad,
+                                         mkdir=False,
+                                         as_int16=self.as_int16,
+                                         device=self.device,
+                                         fill_missing=True)
+        corrected_fields = PairwiseFields(self.corrected_path, 
+                                         offsets=self.offsets,
+                                         bbox=bbox,
+                                         mip=self.mip,
+                                         pad=self.pad,
+                                         mkdir=False,
+                                         as_int16=self.as_int16,
+                                         device=self.device,
+                                         fill_missing=True)
+        # corrected_weights = PairwiseFields(self.weights_path, 
+        #                                  offsets=self.offsets,
+        #                                  bbox=bbox,
+        #                                  mip=mip)
+
+        print("\nFieldsVote\n"
+                "estimated_fields {}\n"
+                "corrected_fields {}\n"
+                "weights {}\n"
+                "src_z {}\n"
+                "tgt_offsets {}\n"
+                "MIP{}\n"
+                "softmin_temp {}\n"
+                "blur_sigma {}\n".format(self.estimates_path, 
+                                    self.corrected_path, 
+                                    self.weights_path,
+                                    self.src_z, 
+                                    self.tgt_offsets, 
+                                    self.mip, 
+                                    self.softmin_temp,
+                                    self.blur_sigma), flush=True)
+        start = time()
+        offsets = np.array(self.tgt_offsets)
+        for k in offsets:
+            # 3-way vector voting to nearest sections, with randomization to break tie
+            estimates = [estimated_fields[(z+k, z)]]
+            random_offsets = np.random.permutation(offsets)
+            intermediaries = random_offsets[np.argsort(np.abs(random_offsets - k))[1:3]]
+            print('Voting for F[{}]'.format((z+k, z)))
+            for j in intermediaries:
+                print('Using fields F[{}]'.format((z+k, z+j, z)))
+                # f_{z+k \leftarrow z+j} \circ f_{z+j \leftarrow z}
+                f = estimated_fields[(z+k, z+j, z)]
+                estimates.append(f)
+
+            estimates = torch.cat([f.field for f in estimates]).field()
+            weights = estimates.voting_weights(softmin_temp=self.softmin_temp,
+                                                  blur_sigma=self.blur_sigma)
+            partition = weights.sum(dim=0, keepdim=True)
+            weights = weights / partition
+            field = (estimates * weights.unsqueeze(-3)).sum(dim=0, keepdim=True)
+            corrected_fields[(z+k, z)] = field
+            # corrected_weights[(z+k, z)] = weights
+
+        end = time()
+        diff = end - start
+        print('FieldsVoteTask: {:.3f} s'.format(diff))
 
--- inference/test_pairwise_fields.py
+++ inference/test_pairwise_fields.py
@@ -7,7 +7,7 @@ from boundingbox import BoundingBox, BoundingCube
 from cloudvolume.lib import Vec
 
 from fields import Field, FieldCloudVolume
-from pairwise_fields import PairwiseFields 
+from pairwise_fields import PairwiseFields, PairwiseVoteTask 
 
 import shutil
 import os
@@ -16,7 +16,7 @@ def delete_layer(path):
     if os.path.exists(path):
         shutil.rmtree(path)  
 
-def test_cloudsample():
+def test_pairwise_fields():
     mip=0
     sz = Vec(*[4, 4, 1])
     info = FieldCloudVolume.create_new_info(
@@ -88,5 +88,107 @@ def test_cloudsample():
     delete_layer('/tmp/cloudvolume/empty_volume')
 
 
+def test_pairwise_vote_task():
+    mip=0
+    sz = Vec(*[4, 4, 1])
+    info = FieldCloudVolume.create_new_info(
+             num_channels=2, 
+             layer_type='image', 
+             data_type='float32', 
+             encoding='raw',
+             resolution=[ 1,1,1 ], 
+             voxel_offset=[0,0,0], 
+             volume_size=[8,8,4],
+             chunk_size=sz,
+             )
 
+    def get_field(sz, x_block, y_block):
+        bbox = BoundingBox(xs=x_block*sz, xe=(x_block+1)*sz, 
+                           ys=y_block*sz, ye=(y_block+1)*sz, 
+                           mip=mip, max_mip=4)
+        data = np.ones((1, 2, sz, sz)) 
+        return Field(data, bbox=bbox)
+
+    f = get_field(sz=4, x_block=0, y_block=0)
+    offsets = [-3,-2,-1,1,2,3]
+    estimates_path = 'file:///tmp/cloudvolume/estimates'
+    F = PairwiseFields(path=estimates_path,
+                       offsets=offsets,
+                       bbox=f.bbox,
+                       mip=mip,
+                       pad=0,
+                       mkdir=True,
+                       as_int16=False,
+                       device='cpu',
+                       info=info,
+                       fill_missing=False)
+    corrected_path = 'file:///tmp/cloudvolume/corrected'
+    C = PairwiseFields(path=corrected_path,
+                       offsets=offsets,
+                       bbox=f.bbox,
+                       mip=mip,
+                       pad=0,
+                       mkdir=True,
+                       as_int16=False,
+                       device='cpu',
+                       info=info,
+                       fill_missing=False)
+
+    # Want vector voting to be on three vectors that are each rotated by 2*\pi / 3
+    # Translate so that average is (0, 1)
+
+    # F[(1, 0)] = (0,4)
+    f = get_field(sz=4, x_block=0, y_block=0)
+    f_cube = BoundingCube.from_bbox(f.bbox, zs=0)
+    f.field.x = 0 
+    f.field.y = 4
+    # convention: PairwiseField.cvs[OFFSET][MIP][BoundingCube]
+    F.cvs[1][mip][f_cube] = f 
+    # F[(1, 2)] = (0,4)
+    f_cube = BoundingCube.from_bbox(f.bbox, zs=2)
+    F.cvs[-1][mip][f_cube] = f 
+    # F[(1, 3)] = (0,4)
+    f_cube = BoundingCube.from_bbox(f.bbox, zs=3)
+    F.cvs[-2][mip][f_cube] = f 
+    # F[(2, 0), new_bbox] = (2*sqrt(3), -2-3)
+    # F[(1, 2, 0)] = (2*sqrt(3), -2)
+    f = get_field(sz=4, x_block=0, y_block=1)
+    f_cube = BoundingCube.from_bbox(f.bbox, zs=0)
+    f.field.x = 2*np.sqrt(3)
+    f.field.y = -5
+    F.cvs[2][mip][f_cube] = f 
+    # F[(3, 0), new_bbox] = (-2*sqrt(3), -2-3)
+    # F[(1, 3, 0)] = (-2*sqrt(3), -2)
+    f = get_field(sz=4, x_block=0, y_block=1)
+    f_cube = BoundingCube.from_bbox(f.bbox, zs=0)
+    f.field.x = -2*np.sqrt(3)
+    f.field.y = -5
+    F.cvs[3][mip][f_cube] = f 
+
+    f = get_field(sz=4, x_block=0, y_block=0)
+    VoteTask = PairwiseVoteTask(estimates_path=estimates_path,
+                                 corrected_path=corrected_path,
+                                 weights_path='',
+                                 offsets=offsets,
+                                 src_z=0,
+                                 tgt_offsets=[1,2,3],
+                                 bbox=f.bbox.serialize(),
+                                 mip=mip,
+                                 pad=0,
+                                 as_int16=False,
+                                 device='cpu',
+                                 softmin_temp=10000, # make all vectors equal
+                                 blur_sigma=1)
+    # We'll vote for F[(1,0)], but the rest will have errors
+    # with pytest.raises(ValueError) as e:
+    VoteTask.execute()
+
+    o = get_field(sz=4, x_block=0, y_block=0)
+    o.field.x = 0 
+    o.field.y = 2/3
+    x = C[(1,0)]
+    assert(o.allclose(x, atol=1e-3))
+
+    delete_layer('/tmp/cloudvolume/estimates')
+    delete_layer('/tmp/cloudvolume/corrected')
 
